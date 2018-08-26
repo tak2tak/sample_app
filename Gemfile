395a5f55b2df04c82ecdaabc0a10624f6d5acaa6
tak2tak <into@bokunomad.com>
2018-08-26 13:40:07 +0000

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -38,10 +38,13 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bootstrap-sass', '3.3.7'
 gem 'jquery-rails'
 gem 'faker', '1.7.3'
+gem 'carrierwave', '1.2.2'
+gem 'mini_magick', '4.7.0'
 gem 'will_paginate', '3.1.6'
 gem 'bootstrap-will_paginate', '1.0.0'
 group :production do
   gem 'pg', '0.20.0'
+  gem 'fog', '1.42'
 end
 
 group :development, :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def create
@@ -55,15 +56,6 @@ class UsersController < ApplicationController
     end
     
     #beforeアクション
-    
-    #ログイン済みユーザーかどうか判断
-    def logged_in_user
-      unless logged_in?
-      store_location
-        flash[:danger] = "Please log in"
-        redirect_to login_url
-      end
-    end
     
     #正しいユーザーか確認
     def correct_user
